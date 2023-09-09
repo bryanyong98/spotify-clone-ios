d80519f6e3ee97160945e01edafac45781869cf2
Bryan Yong <bryanyong@Bryans-MacBook-Pro.local>
2023-09-09 17:16:58 +0800

    Set up user authentication

--- Spotify Clone/Spotify Clone/Controllers/Other/AuthViewController.swift
+++ Spotify Clone/Spotify Clone/Controllers/Other/AuthViewController.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import WebKit
+
+class AuthViewController: UIViewController, WKNavigationDelegate {
+
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        return webView
+    }()
+
+    public var completionHandler: ((Bool) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sign In"
+        view.backgroundColor = .systemBackground
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        guard let url = AuthManager.shared.signInURL else { return }
+        webView.load(URLRequest(url: url))
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else { return }
+
+        // Exchange code for access token
+        let component = URLComponents(string: url.absoluteString)
+        guard let code = component?.queryItems?.first(where: { $0.name == "code" })?.value else {
+            return
+        }
+        print("Code: \(code)")
+    }
+}
--- Spotify Clone/Spotify Clone/Managers/AuthManager.swift
+++ Spotify Clone/Spotify Clone/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
 
+    struct Constants {
+        static let clientID = "88600196bc9c45609b4947359f6c0e7a"
+        static let clientSecret = "924cff0de9c042dcb72fcd0f96465e5f"
+    }
+
     private init() {}
+
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
 
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,20 @@ final class AuthManager {
 
     private var shouldRefreshToken: Bool {
         return false
+    }
+
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get token
+    }
+
+    private func refreshAccessToken() {
+        
+    }
+
+    private func cacheToken() {
+        
     }
 }
